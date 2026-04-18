112e30a9d6b0881928252fc5b62de4405300cf45
Adam Casey <caseyaj05@gmail.com>
2026-04-18 22:46:44 +0100

    added news API to news page

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,13 +1,26 @@
-import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { Component, OnInit } from '@angular/core';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { NewsService } from '../services/news';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner, CommonModule],
 })
-export class Tab1Page {
-  constructor() {}
+export class Tab1Page implements OnInit {
+
+  newsArticles: any[] = [];
+  isLoading = true;
+
+  constructor(private newsService: NewsService) {}
+
+  ngOnInit() {
+    this.newsService.getSoccerNews().subscribe((data: any) => {
+      this.newsArticles = data.articles;
+      this.isLoading = false;
+    });
+  }
+
 }
