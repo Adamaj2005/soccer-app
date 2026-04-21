faf2817d80f0276be42a445d0cd6f20c2b4f9644
Adam Casey <caseyaj05@gmail.com>
2026-04-21 20:55:46 +0100

    added Ionic Storage for saving articles

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,26 +1,44 @@
 import { Component, OnInit } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner } from '@ionic/angular/standalone';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner, IonButton, IonIcon } from '@ionic/angular/standalone';
 import { CommonModule } from '@angular/common';
 import { NewsService } from '../services/news';
+import { Storage } from '@ionic/storage-angular';
+import { addIcons } from 'ionicons';
+import { bookmarkOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
   styleUrls: ['tab1.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner, CommonModule],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonSpinner, IonButton, IonIcon, CommonModule],
 })
 export class Tab1Page implements OnInit {
 
   newsArticles: any[] = [];
   isLoading = true;
 
-  constructor(private newsService: NewsService) {}
+  constructor(private newsService: NewsService, private storage: Storage) {
+    addIcons({ bookmarkOutline });
+  }
 
-  ngOnInit() {
+  async ngOnInit() {
+    await this.storage.create();
     this.newsService.getSoccerNews().subscribe((data: any) => {
       this.newsArticles = data.articles;
       this.isLoading = false;
     });
+  }
+
+  async saveArticle(article: any) {
+    const saved = await this.storage.get('savedArticles') || [];
+    const exists = saved.find((a: any) => a.url === article.url);
+    if (!exists) {
+      saved.push(article);
+      await this.storage.set('savedArticles', saved);
+      alert('Article saved!');
+    } else {
+      alert('Already saved!');
+    }
   }
 
 }
--- src/app/tab3/tab3.page.ts
+++ src/app/tab3/tab3.page.ts
@@ -1,13 +1,32 @@
 import { Component } from '@angular/core';
-import { IonHeader, IonToolbar, IonTitle, IonContent } from '@ionic/angular/standalone';
-import { ExploreContainerComponent } from '../explore-container/explore-container.component';
+import { IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonButton, IonIcon } from '@ionic/angular/standalone';
+import { CommonModule } from '@angular/common';
+import { Storage } from '@ionic/storage-angular';
+import { addIcons } from 'ionicons';
+import { trashOutline } from 'ionicons/icons';
 
 @Component({
   selector: 'app-tab3',
   templateUrl: 'tab3.page.html',
   styleUrls: ['tab3.page.scss'],
-  imports: [IonHeader, IonToolbar, IonTitle, IonContent, ExploreContainerComponent],
+  imports: [IonHeader, IonToolbar, IonTitle, IonContent, IonList, IonItem, IonLabel, IonThumbnail, IonButton, IonIcon, CommonModule],
 })
 export class Tab3Page {
-  constructor() {}
+
+  savedArticles: any[] = [];
+
+  constructor(private storage: Storage) {
+    addIcons({ trashOutline });
+  }
+
+  async ionViewWillEnter() {
+    await this.storage.create();
+    this.savedArticles = await this.storage.get('savedArticles') || [];
+  }
+
+  async removeArticle(article: any) {
+    this.savedArticles = this.savedArticles.filter((a: any) => a.url !== article.url);
+    await this.storage.set('savedArticles', this.savedArticles);
+  }
+
 }
